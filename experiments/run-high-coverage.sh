913a93fb66cc8dc868c9ba4a30642334acc8a5db
edo <Emilio.Dorigatti@stat.uni-muenchen.de>
2019-08-21 17:38:53 +0200

    comparison with popcover

--- experiments/run-high-coverage.sh
+++ experiments/run-high-coverage.sh
@@ -2,14 +2,22 @@
 set -ex
 
 for dir in experiments/results/nef-300-*; do
-    make string-of-beads optitope \
+    make popcover \
+        CONFIG="experiments/base-config.mak" \
+        BASE_DIR="${dir%/}" \
+        POPCOVER_OPTS="--epitopes 10" \
+        POPCOVER_NAME="popcover-high-coverage"
+
+    conservation=$(csvcut ${dir}/made-popcover-vaccine-evaluation.csv -c conservation | tail -n 1)
+    min_proteins=$(csvcut ${dir}/made-popcover-vaccine-evaluation.csv -c prot_coverage | tail -n 1)
+    alleles=$(csvcut ${dir}/made-popcover-vaccine-evaluation.csv -c alleles | tail -n 1)
+
+    make string-of-beads \
         BASE_DIR="${dir%/}" \
         CONFIG="experiments/base-config.mak" \
         CLEAVAGES_OPTS="--top-proteins 500 --top-alleles 500 --top-immunogen 500 --penalty 0.1 --cleavage-window 5 --cleavage-model PCM" \
-        STROBE_OPTS="--max-epitopes 10 --max-aminoacids 0 --cocktail 1 --min-alleles 0.8 --min-proteins 0.95 --min-conservation 0.42" \
-        STROBE_NAME="strobe-high-coverage" \
-        OPTITOPE_OPTS="--epitopes 10 --min-alleles 0.8 --min-proteins 0.95" \
-        OPTITOPE_NAME="optitope-high-coverage"
+        STROBE_OPTS="--max-epitopes 10 --max-aminoacids 0 --cocktail 1 --min-alleles $alleles --min-proteins $min_proteins --min-avg-prot-conservation $conservation" \
+        STROBE_NAME="strobe-high-coverage"
 done
 
 python evaluation.py -v aggregate \

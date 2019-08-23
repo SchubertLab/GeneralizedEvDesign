e0cee17b5a220bbab118490dbeb0ef7df87d4e8c
edo <Emilio.Dorigatti@stat.uni-muenchen.de>
2019-08-23 13:41:02 +0200

    using overlaps

--- experiments/run-cocktail.sh
+++ experiments/run-cocktail.sh
@@ -5,5 +5,5 @@ make CONFIG="experiments/base-config.mak" \
     proteins="experiments/resources/hiv1-bc-nef.fasta" \
     alleles="experiments/resources/alleles.csv" \
     OPTITOPE_OPTS="--min-alleles 0.99 --min-proteins 0.99 --epitopes 24" \
-    MOSAIC_OPTS="--max-aminoacids 36 --max-epitopes 0 --cocktail 6 --top-immunogen 400 --top-proteins 500 --top-alleles 300 --min-alleles 0.99 --min-proteins 0.99" \
+    MOSAIC_OPTS="--max-aminoacids 54 --max-epitopes 0 --cocktail 4 --top-immunogen 0 --top-proteins 25000 --top-alleles 0 --min-proteins 0.99 --min-overlap 8 --greedy-subtour" \
     optitope mosaic

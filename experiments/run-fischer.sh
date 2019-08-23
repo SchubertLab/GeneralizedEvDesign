5ed56d6ba921adebeaf339ee87e45ad644b4eb44
edo <Emilio.Dorigatti@stat.uni-muenchen.de>
2019-08-23 13:37:27 +0200

    evaluating on full protein set

--- experiments/run-fischer.sh
+++ experiments/run-fischer.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -ex
 
-for dir in experiments/results/nef-300-*; do
+# PREREQUISITE: run their online tool on each sample and put the result in fischer.fasta
 
-    # run their online tool on each sample and put the result in fischer.fasta
+for dir in experiments/results/nef-300-*; do
+    # parse fischer's vaccine into our csv format
     python parse_fischer.py -v -l "${dir}/mosaic-fischer-vaccine.log" \
         "${dir}/made-coverage.csv" "${dir}/fischer.fasta" "${dir}/mosaic-fischer-vaccine.csv"
     
+    # evaluate fischer's vaccine
     python evaluation.py vaccine \
         "${dir}/made-proteins.fasta" \
         "${dir}/made-coverage.csv" \
@@ -15,19 +17,36 @@ for dir in experiments/results/nef-300-*; do
         "${dir}/mosaic-fischer-vaccine.csv" \
         "${dir}/mosaic-fischer-vaccine-evaluation.csv"
 
+    # also evaluate on the full set
+    python evaluation.py vaccine \
+        "experiments/results/hiv1bc-full/made-proteins.fasta" \
+        "experiments/results/hiv1bc-full/made-coverage.csv" \
+        "experiments/results/hiv1bc-full/made-alleles.csv" \
+        "experiments/results/hiv1bc-full/made-epitopes.csv" \
+        "${dir}/mosaic-fischer-vaccine.csv" \
+        "${dir}/mosaic-fischer-val-vaccine-evaluation.csv"
+
+    # extract properties
     let aminoacids=$(wc -l ${dir}/mosaic-fischer-vaccine.csv | cut -f 1 -d' ')-1+8
     conservation=$(csvcut ${dir}/mosaic-fischer-vaccine-evaluation.csv -c conservation | tail -n 1)
     min_proteins=$(csvcut ${dir}/mosaic-fischer-vaccine-evaluation.csv -c prot_coverage | tail -n 1)
 
+    # run mosaic to match
     make mosaic \
         BASE_DIR="${dir%/}" \
         MOSAIC_OPTS="--min-overlap 8 --max-epitopes 0 --max-aminoacids $aminoacids --greedy-subtour --min-proteins $min_proteins --min-avg-prot-conservation $conservation" \
         MOSAIC_NAME="mosaic-genev-vaccine"
+    
+    # also evaluate on the full set
+    make mosaic \
+        BASE_DIR="experiments/results/hiv1bc-full" \
+        MOSAIC_VACCINE="${dir}/mosaic-genev-vaccine.csv" \
+        MOSAIC_EVAL="${dir}/mosaic-genev-val-vaccine-evaluation.csv"
 
 done
 
 python evaluation.py -v aggregate \
     'experiments/results/nef-*/mosaic-*-vaccine-evaluation.csv' \
-    --path-format 'experiments/results/nef-300-(?P<rep>\d+)/mosaic-(?P<vax>\w+)-vaccine-evaluation.csv' \
+    --path-format 'experiments/results/nef-300-(?P<rep>\d+)/mosaic-(?P<vax>[a-z\-]+)-vaccine-evaluation.csv' \
     --summary-by vax \
     --output-summary 'experiments/results/mosaic-fischer-evaluation-aggregate.csv'
--- experiments/run-high-coverage.sh
+++ experiments/run-high-coverage.sh
@@ -2,26 +2,42 @@
 set -ex
 
 for dir in experiments/results/nef-300-*; do
+    # design vaccine with popcover
     make popcover \
         CONFIG="experiments/base-config.mak" \
         BASE_DIR="${dir%/}" \
         POPCOVER_OPTS="--epitopes 10" \
         POPCOVER_NAME="popcover-high-coverage"
 
-    conservation=$(csvcut ${dir}/made-popcover-vaccine-evaluation.csv -c conservation | tail -n 1)
-    min_proteins=$(csvcut ${dir}/made-popcover-vaccine-evaluation.csv -c prot_coverage | tail -n 1)
-    alleles=$(csvcut ${dir}/made-popcover-vaccine-evaluation.csv -c alleles | tail -n 1)
+    # find optimal results
+    conservation=$(csvcut ${dir}/popcover-high-coverage-evaluation.csv -c conservation | tail -n 1)
+    min_proteins=$(csvcut ${dir}/popcover-high-coverage-evaluation.csv -c prot_coverage | tail -n 1)
+    alleles=$(csvcut ${dir}/popcover-high-coverage-evaluation.csv -c alleles | tail -n 1)
 
+    # design string-of-beads matching optimal coverage/conservation, optimizing for immunog
     make string-of-beads \
         BASE_DIR="${dir%/}" \
         CONFIG="experiments/base-config.mak" \
         CLEAVAGES_OPTS="--top-proteins 500 --top-alleles 500 --top-immunogen 500 --penalty 0.1 --cleavage-window 5 --cleavage-model PCM" \
         STROBE_OPTS="--max-epitopes 10 --max-aminoacids 0 --cocktail 1 --min-alleles $alleles --min-proteins $min_proteins --min-avg-prot-conservation $conservation" \
         STROBE_NAME="strobe-high-coverage"
+    
+    # also evaluate on the full set
+    make string-of-beads \
+        BASE_DIR="experiments/results/hiv1bc-full" \
+        STROBE_VACCINE="${dir}/strobe-high-coverage.csv" \
+        STROBE_EVAL="${dir}/strobe-val-high-coverage-evaluation.csv"
+    
+    #make string-of-beads \
+    #    BASE_DIR="${dir%/}" \
+    #    CONFIG="experiments/base-config.mak" \
+    #    CLEAVAGES_OPTS="--top-proteins 500 --top-alleles 500 --top-immunogen 500 --penalty 0.1 --cleavage-window 5 --cleavage-model PCM" \
+    #    STROBE_OPTS="--max-epitopes 10 --max-aminoacids 0 --cocktail 1" \
+    #    STROBE_NAME="strobe-high-ig-high-coverage"
 done
 
 python evaluation.py -v aggregate \
     'experiments/results/nef-*/*-high-coverage-evaluation.csv' \
-    --path-format 'experiments/results/nef-300-(?P<rep>\d+)/(?P<vax>\w+)-high-coverage-evaluation.csv' \
+    --path-format 'experiments/results/nef-300-(?P<rep>\d+)/(?P<vax>[a-z\-]+)-high-coverage-evaluation.csv' \
     --summary-by vax \
     --output-summary 'experiments/results/high-coverage-evaluation-aggregate.csv'

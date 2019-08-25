68a9c2313b399ff6b43be78059f9a354cf2582e8
edo <Emilio.Dorigatti@stat.uni-muenchen.de>
2019-08-25 10:50:04 +0200

    cocktail runnable

--- experiments/run-cocktail.sh
+++ experiments/run-cocktail.sh
@@ -1,9 +1,8 @@
-mkdir -p experiments/results/hiv1bc-full
-
 make CONFIG="experiments/base-config.mak" \
     BASE_DIR="experiments/results/hiv1bc-full" \
     proteins="experiments/resources/hiv1-bc-nef.fasta" \
     alleles="experiments/resources/alleles.csv" \
     OPTITOPE_OPTS="--min-alleles 0.99 --min-proteins 0.99 --epitopes 24" \
-    MOSAIC_OPTS="--max-aminoacids 54 --max-epitopes 0 --cocktail 4 --top-immunogen 0 --top-proteins 25000 --top-alleles 0 --min-proteins 0.99 --min-overlap 8 --greedy-subtour" \
+    MOSAIC_OPTS="--max-aminoacids 45 --max-epitopes 0 --cocktail 6 --top-immunogen 500 --top-proteins 500 --top-alleles 0 --min-proteins 0.99 --min-overlap 8 --greedy-subtour" \
+    MOSAIC_NAME="mosaic-6cocktail" \
     optitope mosaic

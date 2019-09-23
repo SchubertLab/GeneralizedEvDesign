2dbba515fe897a9d4088be27c2deba30c0c5a8fc
edo <Emilio.Dorigatti@stat.uni-muenchen.de>
2019-09-23 16:39:58 +0200

    fixed property, not redoing work

--- experiments/run-pareto.sh
+++ experiments/run-pareto.sh
@@ -4,16 +4,19 @@ set -ex
 for dir in experiments/results/nef-300-*; do
     make epitopes cleavages \
         CONFIG="experiments/base-config.mak" \
-        CLEAVAGEs_OPTS="--top-immunogen 1500" \
+        CLEAVAGES_OPTS="--top-immunogen 1500" \
+        CLEAVAGES_NAME="cleavages-1500ig" \
         BASE_DIR="${dir%/}" \
 
+    if [ ! -f "${dir}/made-tradeoff.csv" ]; then
     python tradeoff.py \
         "${dir}/made-epitopes.csv" \
-        "${dir}/made-cleavages.csv" \
+        "${dir}/cleavages-1500ig.csv" \
         "${dir}/made-tradeoff.csv" \
         --log-file "${dir}/made-tradeoff.log" \
         --pareto-steps 11 \
         --max-aminoacids 0 --max-epitopes 10 \
         --min-alleles 0 --min-proteins 0 \
         --cocktail 1 --verbose
+    fi
 done

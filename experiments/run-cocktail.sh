4845bc3e70dc70a2b62547f5479b8b988c618a36
edo <Emilio.Dorigatti@stat.uni-muenchen.de>
2019-09-24 14:26:42 +0200

    cocktail with 4 chains

--- experiments/run-cocktail.sh
+++ experiments/run-cocktail.sh
@@ -3,6 +3,8 @@ make CONFIG="experiments/base-config.mak" \
     proteins="experiments/resources/hiv1-bc-nef.fasta" \
     alleles="experiments/resources/alleles.csv" \
     OPTITOPE_OPTS="--min-alleles 0.99 --min-proteins 0.99 --epitopes 24" \
-    MOSAIC_OPTS="--max-aminoacids 45 --max-epitopes 0 --cocktail 6 --top-immunogen 500 --top-proteins 500 --top-alleles 0 --min-proteins 0.99 --min-overlap 8 --greedy-subtour" \
-    MOSAIC_NAME="mosaic-6cocktail" \
+    OPTITOPE_NAME="optitope-p99" \
+    MOSAIC_OPTS="--max-aminoacids 54 --max-epitopes 0 --cocktail 4 --top-immunogen 1000 --top-proteins 1000 --top-alleles 0 --min-proteins 0.99 --min-overlap 8 --greedy-subtour" \
+    MOSAIC_NAME="mosaic-4cocktail" \
+    MOSAIC_EVAL_LOG_NAME="mosaic-4cocktail-evaluation" \
     optitope mosaic

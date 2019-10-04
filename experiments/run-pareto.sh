02591de8041b856269f7ac96dde111fbad924915
edo <Emilio.Dorigatti@stat.uni-muenchen.de>
2019-10-04 14:32:00 +0200

    indent

--- experiments/run-pareto.sh
+++ experiments/run-pareto.sh
@@ -9,14 +9,14 @@ for dir in experiments/results/nef-300-*; do
         BASE_DIR="${dir%/}" \
 
     if [ ! -f "${dir}/made-tradeoff.csv" ]; then
-    python tradeoff.py \
-        "${dir}/made-epitopes.csv" \
-        "${dir}/cleavages-1500ig.csv" \
-        "${dir}/made-tradeoff.csv" \
-        --log-file "${dir}/made-tradeoff.log" \
-        --pareto-steps 11 \
-        --max-aminoacids 0 --max-epitopes 10 \
-        --min-alleles 0 --min-proteins 0 \
-        --cocktail 1 --verbose
+        python tradeoff.py \
+            "${dir}/made-epitopes.csv" \
+            "${dir}/cleavages-1500ig.csv" \
+            "${dir}/made-tradeoff.csv" \
+            --log-file "${dir}/made-tradeoff.log" \
+            --pareto-steps 11 \
+            --max-aminoacids 0 --max-epitopes 10 \
+            --min-alleles 0 --min-proteins 0 \
+            --cocktail 1 --verbose
     fi
 done

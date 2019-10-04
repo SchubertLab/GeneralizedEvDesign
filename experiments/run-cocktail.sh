2ba72e70af600f968a0f0d5f8c713d1d5c46b6d4
edo <Emilio.Dorigatti@stat.uni-muenchen.de>
2019-10-04 14:30:38 +0200

    considering more epitopes and unconstrained baseline

--- experiments/run-cocktail.sh
+++ experiments/run-cocktail.sh
@@ -4,7 +4,12 @@ make CONFIG="experiments/base-config.mak" \
     alleles="experiments/resources/alleles.csv" \
     OPTITOPE_OPTS="--min-alleles 0.99 --min-proteins 0.99 --epitopes 24" \
     OPTITOPE_NAME="optitope-p99" \
-    MOSAIC_OPTS="--max-aminoacids 54 --max-epitopes 0 --cocktail 4 --top-immunogen 1000 --top-proteins 1000 --top-alleles 0 --min-proteins 0.99 --min-overlap 8 --greedy-subtour" \
+    MOSAIC_OPTS="--max-aminoacids 54 --max-epitopes 0 --cocktail 4 --top-immunogen 2000 --top-proteins 2000 --top-alleles 0 --min-proteins 0.99 --min-overlap 8 --greedy-subtour" \
     MOSAIC_NAME="mosaic-4cocktail" \
     MOSAIC_EVAL_LOG_NAME="mosaic-4cocktail-evaluation" \
     optitope mosaic
+
+make mosaic \
+    BASE_DIR="experiments/results/hiv1bc-full"\
+    MOSAIC_NAME="mosaic-highig" \
+    MOSAIC_OPTS="--min-overlap 8 --greedy-subtour --max-epitopes 0 --max-aminoacids 216 --top-immunogen 10000 --top-proteins 10000"

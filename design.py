6a62fd33c33f7ea71a8fdd4a5ec45beb67317bf7
edo <Emilio.Dorigatti@stat.uni-muenchen.de>
2019-11-18 09:07:51 +0100

    typo

--- design.py
+++ design.py
@@ -6,7 +6,6 @@ import utilities
 import logging
 import os
 import time
-from builtins import map
 from collections import defaultdict
 from random import sample as random_sample
 
@@ -19,9 +18,8 @@ from Fred2.Core import (Allele, Peptide, Protein,
 from Fred2.Core.Peptide import Peptide
 from Fred2.EpitopePrediction import (EpitopePredictionResult,
                                      EpitopePredictorFactory)
-from Fred2.EpitopeSelection import OptiTope, PopCover
+from Fred2.EpitopeSelection import OptiTope #, PopCover
 from Fred2.IO import FileReader
-from Fred2.Utility import generate_overlap_graph
 
 from mosaic_vaccine_ilp import (DataContainer, EvaluationResult,
                                 MosaicVaccineILP)
@@ -232,7 +230,7 @@ def optitope(input_affinities, input_peptides, input_alleles, output_vaccine, ep
             epitope_immunog = sum(model.instance.p[a] * model.instance.i[epitope, a]
                                   for a in model.instance.A)
             total_ig += epitope_immunog
-            LOGGER.info('    %s -logger.2f', epitope, epitope_immunog)
+            LOGGER.info('    %s - %.2f', epitope, epitope_immunog)
         LOGGER.info('Total immunogenicity: %.2f', total_ig)
 
 

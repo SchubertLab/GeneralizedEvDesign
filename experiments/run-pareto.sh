8c142211482182bea89502fe4b63c972f9b9eb66
e-dorigatti <emilio.dorigatti@gmail.com>
2020-06-18 12:03:15 +0200

    pareto experiment with spacers

--- experiments/run-pareto.sh
+++ experiments/run-pareto.sh
@@ -2,6 +2,7 @@
 set -ex
 
 for dir in experiments/results/nef-300-*; do
+    # pareto without spacers
     make pareto \
         CONFIG="experiments/base-config.mak" \
         CLEAVAGES_NAME="cleavages-1500ig" \
@@ -9,4 +10,17 @@ for dir in experiments/results/nef-300-*; do
         PARETO_NAME="made-tradeoff" \
         PARETO_OPTS="--pareto-steps 11 --max-aminoacids 0 --max-epitopes 10" \
         BASE_DIR="${dir%/}"
+
+    # pareto with spacers
+    make spacers \
+         SPACERS_NAME="spacers-1500ig" \
+         SPACERS_OPTS="-l 0 -l 4 --top-immunogen 1500" \
+         BASE_DIR="${dir%/}"
+
+    make pareto \
+         CONFIG="experiments/base-config.mak" \
+         CLEAVAGES_NAME="spacers-1500ig" \
+         PARETO_NAME="made-spacers-tradeoff" \
+         PARETO_OPTS="--pareto-steps 11 --max-aminoacids 0 --max-epitopes 10" \
+         BASE_DIR="${dir%/}"
 done
